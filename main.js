c69106b99d2e88f5e55e71a7facfb19005f3e8ab
seekeryang7 <54164340+seekeryang7@users.noreply.github.com>
2019-10-01 19:01:48 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+// function play() {
+//     let audio = document.getElementById("audio");
+//     audio.play();
+// }
+
+function play_A() {
+    let audio= document.getElementById("audio_A");
+    audio.play();
+}
+ function play_S() {
+     let audio= document.getElementById("audio_S");
+     audio.play();
+ }
+ function play_D() {
+     let audio= document.getElementById("audio_D");
+     audio.play();
+ }
+ function play_F(){
+     let audio= document.getElementById("audio_F");
+     audio.play();
+ }
+ function play_G() {
+     let audio= document.getElementById("audio_G");
+     audio.play();
+ }
+ function play_H() {
+     let audio= document.getElementById("audio_H");
+     audio.play();
+ }
+ function play_J() {
+     let audio= document.getElementById("audio_J");
+     audio.play();
+ }
+ function play_K() {
+     let audio= document.getElementById("audio_K");
+     audio.play();
+}
+ function play_L() {
+     let audio= document.getElementById("audio_L");
+     audio.play();
+ }
